259dfb57230c314640a4cc77d9700b9d671e3c07
andywoo1001 <cheesehouse.u@gmail.com>
2024-10-06 04:23:25 +0900

    #9.3 render

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
 
-def say_hello(request):
-    return HttpResponse("hello!")
+def see_all_rooms(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "Hello! this title comes from django!",
+        },
+    )
+
+
+def see_one_room(request, room_id):
+    return HttpResponse("See one room")
