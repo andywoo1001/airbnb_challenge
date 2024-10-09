313ffe816d130484a804195b71621582c6be0dd2
andywoo1001 <cheesehouse.u@gmail.com>
2024-10-09 22:49:57 +0900

    #10.12 ModelViewSet

--- categories/views.py
+++ categories/views.py
@@ -5,81 +5,14 @@ from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from .serializers import CategorySerializer
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.viewsets import ModelViewSet
 
 # from django.shortcuts import render
 from .models import Category
 
+
 # Create your views here.
+class CategoryViewSet(ModelViewSet):
 
-
-class Categories(APIView):
-
-    def get(self, request):
-        all_categoreis = Category.objects.all()
-        serializer = CategorySerializer(all_categoreis, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(CategorySerializer(new_category).data)
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise NotFound
-        return category
-
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk),
-            data=request.data,
-            partial=True,
-        )
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
-
-
-# @api_view(["GET", "PUT", "DELETE"])
-# def category(request, pk):
-#     try:
-#         category = Category.objects.get(pk=pk)
-#     except Category.DoesNotExist:
-#         raise NotFound
-
-#     if request.method == "GET":
-#         serializer = CategorySerializer(category)
-#         return Response(serializer.data)
-
-#     elif request.method == "PUT":
-#         serializer = CategorySerializer(
-#             category,
-#             data=request.data,
-#             partial=True,
-#         )
-#         if serializer.is_valid():
-#             updated_category = serializer.save()
-#             return Response(CategorySerializer(updated_category).data)
-#         else:
-#             return Response(serializer.errors)
-#     elif request.method == "DELETE":
-#         category.delete()
-#         return Response(status=HTTP_204_NO_CONTENT)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
